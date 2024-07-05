c25877797638314c444b7c35ee067d8d253ed715
VulpeOlesea <leseavulpe@gmail.com>
2024-07-05 23:14:05 +0100

    add exe10 - Alterar, eliminar, inserir os ficheiros XML

---
+++ WinForm/exe10/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace exe10
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.tb_caminho = new System.Windows.Forms.TextBox();
+            this.btn_carregar = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lst_imagens = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.tb_id = new System.Windows.Forms.TextBox();
+            this.tb_descricao = new System.Windows.Forms.TextBox();
+            this.tb_ficheiro = new System.Windows.Forms.TextBox();
+            this.btn_alterar = new System.Windows.Forms.Button();
+            this.btn_eliminar = new System.Windows.Forms.Button();
+            this.btn_inserir = new System.Windows.Forms.Button();
+            this.btn_total = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.btn_escolher = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(66, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ficheiro:";
+            // 
+            // tb_caminho
+            // 
+            this.tb_caminho.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_caminho.Location = new System.Drawing.Point(191, 81);
+            this.tb_caminho.Name = "tb_caminho";
+            this.tb_caminho.Size = new System.Drawing.Size(455, 26);
+            this.tb_caminho.TabIndex = 1;
+            // 
+            // btn_carregar
+            // 
+            this.btn_carregar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_carregar.Location = new System.Drawing.Point(72, 128);
+            this.btn_carregar.Name = "btn_carregar";
+            this.btn_carregar.Size = new System.Drawing.Size(690, 38);
+            this.btn_carregar.TabIndex = 2;
+            this.btn_carregar.Text = "Carregar XML";
+            this.btn_carregar.UseVisualStyleBackColor = true;
+            this.btn_carregar.Click += new System.EventHandler(this.btn_carregar_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lst_imagens);
+            this.groupBox1.Location = new System.Drawing.Point(72, 203);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(297, 332);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Imagens";
+            // 
+            // lst_imagens
+            // 
+            this.lst_imagens.FormattingEnabled = true;
+            this.lst_imagens.ItemHeight = 16;
+            this.lst_imagens.Location = new System.Drawing.Point(15, 30);
+            this.lst_imagens.Name = "lst_imagens";
+            this.lst_imagens.Size = new System.Drawing.Size(259, 276);
+            this.lst_imagens.TabIndex = 4;
+            this.lst_imagens.SelectedIndexChanged += new System.EventHandler(this.lst_imagem_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(475, 209);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 29);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "ID:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(390, 269);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Descrição:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(410, 328);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(107, 29);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Ficheiro:";
+            // 
+            // tb_id
+            // 
+            this.tb_id.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_id.Location = new System.Drawing.Point(534, 213);
+            this.tb_id.Name = "tb_id";
+            this.tb_id.Size = new System.Drawing.Size(228, 26);
+            this.tb_id.TabIndex = 7;
+            // 
+            // tb_descricao
+            // 
+            this.tb_descricao.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_descricao.Location = new System.Drawing.Point(534, 272);
+            this.tb_descricao.Name = "tb_descricao";
+            this.tb_descricao.Size = new System.Drawing.Size(228, 26);
+            this.tb_descricao.TabIndex = 8;
+            // 
+            // tb_ficheiro
+            // 
+            this.tb_ficheiro.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tb_ficheiro.Location = new System.Drawing.Point(534, 332);
+            this.tb_ficheiro.Name = "tb_ficheiro";
+            this.tb_ficheiro.Size = new System.Drawing.Size(228, 26);
+            this.tb_ficheiro.TabIndex = 9;
+            // 
+            // btn_alterar
+            // 
+            this.btn_alterar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_alterar.Location = new System.Drawing.Point(446, 408);
+            this.btn_alterar.Name = "btn_alterar";
+            this.btn_alterar.Size = new System.Drawing.Size(115, 51);
+            this.btn_alterar.TabIndex = 10;
+            this.btn_alterar.Text = "Alterar";
+            this.btn_alterar.UseVisualStyleBackColor = true;
+            this.btn_alterar.Click += new System.EventHandler(this.btn_alterar_Click);
+            // 
+            // btn_eliminar
+            // 
+            this.btn_eliminar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_eliminar.Location = new System.Drawing.Point(600, 408);
+            this.btn_eliminar.Name = "btn_eliminar";
+            this.btn_eliminar.Size = new System.Drawing.Size(115, 51);
+            this.btn_eliminar.TabIndex = 11;
+            this.btn_eliminar.Text = "Eliminar";
+            this.btn_eliminar.UseVisualStyleBackColor = true;
+            this.btn_eliminar.Click += new System.EventHandler(this.btn_eliminar_Click);
+            // 
+            // btn_inserir
+            // 
+            this.btn_inserir.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_inserir.Location = new System.Drawing.Point(446, 483);
+            this.btn_inserir.Name = "btn_inserir";
+            this.btn_inserir.Size = new System.Drawing.Size(115, 51);
+            this.btn_inserir.TabIndex = 12;
+            this.btn_inserir.Text = "Inserir";
+            this.btn_inserir.UseVisualStyleBackColor = true;
+            this.btn_inserir.Click += new System.EventHandler(this.btn_inserir_Click);
+            // 
+            // btn_total
+            // 
+            this.btn_total.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_total.Location = new System.Drawing.Point(600, 483);
+            this.btn_total.Name = "btn_total";
+            this.btn_total.Size = new System.Drawing.Size(115, 51);
+            this.btn_total.TabIndex = 13;
+            this.btn_total.Text = "№ Total Imagens";
+            this.btn_total.UseVisualStyleBackColor = true;
+            this.btn_total.Click += new System.EventHandler(this.btn_total_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // btn_escolher
+            // 
+            this.btn_escolher.BackColor = System.Drawing.SystemColors.Info;
+            this.btn_escolher.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_escolher.Location = new System.Drawing.Point(664, 76);
+            this.btn_escolher.Name = "btn_escolher";
+            this.btn_escolher.Size = new System.Drawing.Size(98, 33);
+            this.btn_escolher.TabIndex = 14;
+            this.btn_escolher.Text = "Escolher";
+            this.btn_escolher.UseVisualStyleBackColor = false;
+            this.btn_escolher.Click += new System.EventHandler(this.btn_escoler_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 578);
+            this.Controls.Add(this.btn_escolher);
+            this.Controls.Add(this.btn_total);
+            this.Controls.Add(this.btn_inserir);
+            this.Controls.Add(this.btn_eliminar);
+            this.Controls.Add(this.btn_alterar);
+            this.Controls.Add(this.tb_ficheiro);
+            this.Controls.Add(this.tb_descricao);
+            this.Controls.Add(this.tb_id);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_carregar);
+            this.Controls.Add(this.tb_caminho);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tb_caminho;
+        private System.Windows.Forms.Button btn_carregar;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListBox lst_imagens;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox tb_id;
+        private System.Windows.Forms.TextBox tb_descricao;
+        private System.Windows.Forms.TextBox tb_ficheiro;
+        private System.Windows.Forms.Button btn_alterar;
+        private System.Windows.Forms.Button btn_eliminar;
+        private System.Windows.Forms.Button btn_inserir;
+        private System.Windows.Forms.Button btn_total;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button btn_escolher;
+    }
+}
+
---
+++ WinForm/exe10/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace exe10
+{
+    public partial class Form1 : Form
+    {
+        XmlDocument doc;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+         private void btn_escoler_Click(object sender, EventArgs e)
+         {
+            openFileDialog1.Title = "Escolher ficheiro XML";
+            openFileDialog1.Filter = "Ficheiros XML| *. xml";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                tb_caminho.Text = openFileDialog1.FileName;
+            }
+         }
+
+        private void btn_carregar_Click(object sender, EventArgs e)
+        {
+            if (tb_caminho.Text == "")
+            {
+                MessageBox.Show("Atenção, terá de escolher um ficheiro XML primeiro");
+            }
+            else
+            {
+                doc = new XmlDocument();
+                doc.Load(tb_caminho.Text);
+
+                XmlNodeList nodeList = doc.SelectNodes(@"/galeria/imagem");
+
+                lst_imagens.Items.Clear();
+
+                foreach (XmlNode no in nodeList)
+                {
+                    XmlElement elemento = no as XmlElement;
+
+                    lst_imagens.Items.Add(elemento.Attributes.GetNamedItem("ficheiro").Value);
+                }
+            }
+        }
+
+        private void lst_imagem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            XmlNodeList nodeList = doc.SelectNodes(@"/galeria/imagem");
+
+            XmlNode no = nodeList.Item(lst_imagens.SelectedIndex);
+            XmlElement elemento = no as XmlElement;
+
+            tb_id.Text = elemento.Attributes.GetNamedItem("id").Value;
+            tb_descricao.Text = elemento.Attributes.GetNamedItem("descricao").Value;
+            tb_ficheiro.Text = elemento.Attributes.GetNamedItem("ficheiro").Value;
+        }
+
+        private void btn_alterar_Click(object sender, EventArgs e)
+        {
+            XmlNodeList nodeList = doc.SelectNodes(@"/galeria/imagem");
+
+            XmlNode no = nodeList.Item(lst_imagens.SelectedIndex);
+            XmlElement elemento = no as XmlElement;
+
+            elemento.Attributes.GetNamedItem("id").Value = tb_id.Text;
+            elemento.Attributes.GetNamedItem("descricao").Value = tb_descricao.Text;
+            elemento.Attributes.GetNamedItem("ficheiro").Value = tb_ficheiro.Text;
+
+            doc.Save(tb_caminho.Text);
+
+            int posicao = lst_imagens.SelectedIndex;
+
+            // Refrescar os dados da listbox
+
+            XmlNodeList nodeList2 = doc.SelectNodes(@"/galeria/imagem");
+
+            lst_imagens.Items.Clear();
+
+            foreach (XmlNode no2 in nodeList2)
+            {
+                XmlElement elemento2 = no2 as XmlElement;
+
+                lst_imagens.Items.Add(elemento2.Attributes.GetNamedItem("ficheiro").Value);
+            }
+
+            lst_imagens.SelectedIndex = posicao;
+        }
+
+        private void btn_eliminar_Click(object sender, EventArgs e)
+        {
+            XmlNodeList nodeList = doc.SelectNodes(@"/galeria/imagem");
+
+            XmlNode no = nodeList.Item(lst_imagens.SelectedIndex);
+            XmlElement elemento = no as XmlElement;
+            elemento.ParentNode.RemoveChild(no);
+
+            doc.Save(tb_caminho.Text);
+
+            // Regrescar a ListBox
+
+            XmlNodeList nodeList2 = doc.SelectNodes(@"/galeria/imagem");
+
+            lst_imagens.Items.Clear();
+
+            foreach (XmlNode no2 in nodeList2)
+            {
+                XmlElement elemento2 = no2 as XmlElement;
+
+                lst_imagens.Items.Add(elemento2.Attributes.GetNamedItem("ficheiro").Value);
+            }
+
+            tb_id.Text = "";
+            tb_descricao.Text = "";
+            tb_ficheiro.Text = "";
+        }
+
+        private void btn_inserir_Click(object sender, EventArgs e)
+        {
+            XmlNodeList nodeList = doc.SelectNodes(@"/galeria/imagem");
+
+            XmlNode no = doc.SelectSingleNode("/galeria");
+            XmlElement elemento = doc.CreateElement("imagem");
+            elemento.SetAttribute("id", tb_id.Text);
+            elemento.SetAttribute("descricao", tb_descricao.Text);
+            elemento.SetAttribute("ficheiro", tb_ficheiro.Text);
+
+            no.AppendChild(elemento);
+
+            doc.Save(tb_caminho.Text);
+
+            // Regrescar a ListBox
+
+            XmlNodeList nodeList2 = doc.SelectNodes(@"/galeria/imagem");
+
+            lst_imagens.Items.Clear();
+
+            foreach (XmlNode no2 in nodeList2)
+            {
+                XmlElement elemento2 = no2 as XmlElement;
+
+                lst_imagens.Items.Add(elemento2.Attributes.GetNamedItem("ficheiro").Value);
+
+            }
+
+            tb_id.Text = "";
+            tb_descricao.Text = "";
+            tb_ficheiro.Text = "";
+        }
+
+        private void btn_total_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Existem {lst_imagens.Items.Count} imagens no ficheiro.");
+        }
+    }    
+}
